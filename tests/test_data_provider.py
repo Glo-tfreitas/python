4a3cfc995f330296630e68223cb7665975c2ec62
Glo.tfreitas <Glo.tfreitas@iberia.es>
2025-06-17 12:02:39 +0200

    fixed typo

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -2,5 +2,5 @@ import pytest # type: ignore
 from utils.util import get_data
 
 @pytest.mark.parametrize("id, name, course, city", get_data())
-def tet_checkdata_from_file(id, name, course, city):
+def test_checkdata_from_file(id, name, course, city):
     print(city)
